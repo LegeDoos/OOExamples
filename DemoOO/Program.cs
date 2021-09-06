421c2c0f5f6825bddbcf31f5997c75e21faebd7a
Rob Cilissen <rob@cilissen.nl>
2021-09-06 14:41:43 +0200

    Project created

---
+++ DemoOO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
